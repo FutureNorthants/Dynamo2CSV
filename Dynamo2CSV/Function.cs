999f9fd537161892d5b727749a1d51e510c44f95
Kevin White <kwhite@northampton.gov.uk>
2019-06-26 15:57:31 +0100

    Initial structure to extract CXM data to send to Veolia.

---
+++ Dynamo2CSV/Function.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+using Amazon.Lambda.Core;
+using Amazon.S3;
+using Amazon.S3.Model;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace Dynamo2CSV
+{
+    public class Function
+    {
+        private static IAmazonS3 s3Client;
+        public string FunctionHandler(string tableName, ILambdaContext context)
+        {
+            context.Logger.LogLine("Process Started");
+            String functionResult = "";           
+            AmazonDynamoDBConfig clientConfig = new AmazonDynamoDBConfig();
+            clientConfig.RegionEndpoint = RegionEndpoint.EUWest2;
+            AmazonDynamoDBClient client = new AmazonDynamoDBClient(clientConfig);
+            s3Client = new AmazonS3Client(RegionEndpoint.EUWest2);
+            ScanRequest request = new ScanRequest
+            {
+                TableName = tableName,
+            };
+            Task<ScanResponse> response = client.ScanAsync(request);
+            var result = response.Result.Count;
+            String fileContents = "";
+            foreach (Dictionary<string, AttributeValue> item in response.Result.Items)
+            {
+                functionResult += item;
+                context.Logger.LogLine("Case " + item["Reference"].S.ToString());
+                fileContents += item["Reference"].S.ToString() + "," + "\r";
+            }
+            Write2S3Async(context, "cxm2veoliafiles", "myfile", fileContents).Wait();
+            context.Logger.LogLine("Process Finished");
+            return result.ToString();
+        }
+
+        static async Task Write2S3Async(ILambdaContext context, String bucketName, String keyName, String fileContents)
+        {
+            try
+            {
+                // 1. Put object-specify only key name for the new object.
+                var putRequest1 = new PutObjectRequest
+                {
+                    BucketName = bucketName,
+                    Key = keyName,
+                    ContentBody = fileContents
+                };
+
+                PutObjectResponse response1 = await s3Client.PutObjectAsync(putRequest1);
+            }
+            catch (AmazonS3Exception e)
+            {
+                context.Logger.LogLine(
+                        "Error encountered when writing an object"
+                       );
+            }
+            catch (Exception e)
+            {
+                context.Logger.LogLine(
+                    "Unknown encountered on server when writing an object"
+                    );
+            }
+        }
+    }
+}
